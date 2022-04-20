e416e8891e6fb26e2ed472e9ad28bf2768685132
Muhammet <muhammedabuchary@gmail.com>
2022-04-20 13:28:56 -0500

    OfficeHours_JDBC_SQL_Library_USs

--- src/test/java/com/library/pages/LoginPage.java
+++ src/test/java/com/library/pages/LoginPage.java
@@ -22,8 +22,7 @@ public class LoginPage {
     @FindBy(tagName = "button")
     public WebElement loginButton;
 
-    @FindBy(id = "borrowed_books")
-    public WebElement borrowedBooksNumber;
+
 
     public void login(){
 
@@ -37,8 +36,6 @@ public class LoginPage {
         loginButton.click();
 
     }
-
-
 
 
 }
--- src/test/java/com/library/tests/US2.java
+++ src/test/java/com/library/tests/US2.java
@@ -1,8 +1,13 @@
 package com.library.tests;
 
 
+import com.library.pages.DashBoardPage;
+import com.library.pages.LoginPage;
 import com.library.tests.base.TestBase;
 
+import com.library.utility.BrowserUtil;
+import com.library.utility.DB_Util;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class US2 extends TestBase {
@@ -10,10 +15,24 @@ public class US2 extends TestBase {
     @Test
     public void us2test1() {
 
-
+        LoginPage loginPage = new LoginPage();
         // Given I am in the homepage of library app
+        loginPage.login();
+        BrowserUtil.waitFor(2);
 
-        //    When I take borrowed books number
+        // When I take borrowed books number (actual)
+        DashBoardPage dashBoardPage=new DashBoardPage();
+        String actualBorrowBooksNumber = dashBoardPage.borrowedBooksNumber.getText();
+        System.out.println("actualBorrowBooksNumber from UI= " + actualBorrowBooksNumber);
+        // When I take borrowed books number from DB  (expected)
+        DB_Util.runQuery("SELECT COUNT(*)\n" +
+                "FROM book_borrow\n" +
+                "WHERE is_returned = 0");
+        String expectedBorrowBooksNumber = DB_Util.getFirstRowFirstColumn();
+        System.out.println("expectedBorrowBooksNumber from DB = " + expectedBorrowBooksNumber);
+        //    Then borrowed books number information from UI must match with DB
+        Assert.assertEquals(actualBorrowBooksNumber, expectedBorrowBooksNumber);
+
 
         //    Then borrowed books number information must match with DB
 
